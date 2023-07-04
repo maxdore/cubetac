2fe4a00f648c2b65fc8f8d62bb4f16e2d1183431
Maximilian Doré <maximilian.dore@cs.ox.ac.uk>
2023-07-04 15:01:00 +0100

    Cleanup

--- src/Deg.hs
+++ src/Deg.hs
@@ -9,10 +9,12 @@ import Data.Map ((!), Map)
 import Data.Maybe
 import Data.Ord
 
+
+
 -- basic type synonyms
 type Id = String
 type Dim = Int
-type IVar = Int
+type IVar = Int -- De Bruijn indices for variable names
 
 -- Endpoint datatype for nice printing
 data Endpoint = I0 | I1
@@ -22,7 +24,8 @@ negI :: Endpoint -> Endpoint
 negI I0 = I1
 negI I1 = I0
 
--- Term language
+-- TERM LANGUAGE
+
 type Restr = (IVar , Endpoint)
 
 data Term = Var Id
@@ -30,7 +33,6 @@ data Term = Var Id
           | Fill Restr Ty -- Fill dir ty means fill type ty in direction dir
           | Comp Restr Ty
   deriving (Eq , Show)
-
 
 -- Syntactic sugar to allow writing (1,I0) +> t
 (+>) :: Restr -> Term -> Face
@@ -43,7 +45,7 @@ data Ty = Ty Dim [Face]
 instance Eq Ty where
   Ty d fs ==  Ty d' fs' = d == d' && sortOn fst fs == sortOn fst fs'
 
--- Print faces on different lines and indented
+-- Print faces of a cube on different lines and indented
 instance Show Ty where
   show (Ty d fs) = "Ty " ++ show d ++ " [\n  " ++
     intercalate ", " (map
@@ -52,25 +54,24 @@ instance Show Ty where
     ++ "]"
 
 type Decl = (Id , Ty)
-type Cube = [Decl]
+type Ctxt = [Decl]
 
 
 -- Basic operations on the types
-idDim :: Cube -> Id -> Dim
+idDim :: Ctxt -> Id -> Dim
 idDim c p = let Ty d _ = getDef c p in d
 
-termDim :: Cube -> Term -> Dim
+termDim :: Ctxt -> Term -> Dim
 termDim c (Var p) = idDim c p
 termDim c (Deg t _) = 1 + termDim c t
 termDim _ (Fill _ (Ty d _)) = d
 termDim _ (Comp _ (Ty d _)) = d-1
 
-getDef :: Cube -> Id -> Ty
+getDef :: Ctxt -> Id -> Ty
 getDef c name =
   case lookup name c of
     Just face -> face
     Nothing -> error $ "Could not find definition of " ++ name
-
 
 getFace :: Ty -> Restr -> Term
 getFace (Ty _ fs) f =
@@ -78,26 +79,27 @@ getFace (Ty _ fs) f =
     Just face -> face
     Nothing -> error $ "Could not find face " ++ show f
 
+termFace :: Ctxt -> Term -> Restr -> Term
+termFace c t = getFace (inferTy c t)
+
 sideSpec :: Ty -> Restr -> Bool
 sideSpec (Ty _ fs) f = isJust (lookup f fs)
 
-
-validTy :: Cube -> Ty -> Bool
+validTy :: Ctxt -> Ty -> Bool
 validTy c (Ty _ fs) =
   and [ termFace c t (j-1,e') == termFace c t' (i,e)
            | ((i,e) , t) <- fs , ((j,e') , t') <- fs , i < j ]
   -- TODO check also no faces assigned multiple times & that d not exceeded
 
-validTerm :: Cube -> Term -> Bool
+validTerm :: Ctxt -> Term -> Bool
 validTerm c (Fill f ty) = not (sideSpec ty f) && validTy c ty
 
-
-computeOpenBoundary :: Cube -> Ty -> Restr -> Ty
+computeOpenBoundary :: Ctxt -> Ty -> Restr -> Ty
 computeOpenBoundary c (Ty d fs) (oi,oe) =
   Ty (d-1) [ (if i < oi then i else i-1 , e) +> termFace c t (if oi > i then oi-1 else oi,oe) | ((i,e),t) <- fs , i /= oi ]
 
--- type inference
-inferTy :: Cube -> Term -> Ty
+-- Type inference
+inferTy :: Ctxt -> Term -> Ty
 inferTy c (Var p) = getDef c p
 inferTy c (Deg t i) = let Ty d _ = inferTy c t in
   Ty (d+1) (   [ (j,e) +> Deg (termFace c t (j,e)) d | j <- [1..i-1] , e <- [I0,I1] ]
@@ -106,25 +108,24 @@ inferTy c (Deg t i) = let Ty d _ = inferTy c t in
 inferTy _ (Fill o (Ty d fs)) = Ty d ((o +> Comp o (Ty d fs)) : fs)
 inferTy c (Comp o ty) = computeOpenBoundary c ty o
 
-termFace :: Cube -> Term -> Restr -> Term
-termFace c t = getFace (inferTy c t)
-
 
 -- Generate terms for domains
 restrictions :: Int -> [Restr]
 restrictions n = [ (i,e) | i <- [1..n], e <- [I0,I1]]
 
-allVarDeg :: Cube -> Id -> Dim -> [Term]
+-- All degeneracies for a given dimension
+allVarDeg :: Ctxt -> Id -> Dim -> [Term]
 allVarDeg c p d | idDim c p > d = []
                    | idDim c p == d = [Var p]
                    | otherwise =
                        nubBy (\t t' -> inferTy c t == inferTy c t')
                        [ Deg t j | t <- allVarDeg c p (d-1) , j <- [1..d] ]
 
-allDeg :: Cube -> Dim -> [Term]
+allDeg :: Ctxt -> Dim -> [Term]
 allDeg c d = concat [ allVarDeg c p d | (p,_) <- c ]
 
-allFill :: Cube -> Dim -> [Term]
+-- All basic fillers for a given dimension
+allFill :: Ctxt -> Dim -> [Term]
 allFill c d =
   let ts = allDeg c (d-1) in
   let restr = restrictions d in
@@ -140,7 +141,42 @@ allFill c d =
 
 
 
--- Composition solver
+-- MAIN SOLVER LOGIC
+
+-- Call the composition solver by iteratively deepening box level
+findComp :: Ctxt -> Ty -> Term
+findComp c ty = head (concatMap (findCompBounded c ty) [0..])
+
+findCompBounded :: Ctxt -> Ty -> Int -> [Term]
+findCompBounded c ty 0 = constrOpenComp c ty [[]] 0
+findCompBounded c ty@(Ty d _) depth =
+  let sides = restrictions (d + 1) ++ [(d + 1, I0)] in
+  constrOpenComp c ty (incps sides) depth
+    where
+    -- Generates the powerset of a list in ascending cardinality
+    incps :: [a] -> [[a]]
+    incps = sortBy (comparing length) . filterM (const [True, False])
+
+-- opens specifies which combinations of sides of the cube can be left open
+constrOpenComp :: Ctxt -> Ty -> [[Restr]] -> Int -> [Term]
+constrOpenComp c ty@(Ty d _) opens depth = do
+  let cdir = (d + 1, I1)
+  ope <- opens
+  sol <- evalStateT runCS (SEnv c ty cdir ope Map.empty)
+  full <- foldM
+    (\s o -> do
+        let wty = Ty (d + 1) (s ++ [(d + 1 , I1) +> Fill cdir ty])
+        let fty = computeOpenBoundary c wty o
+        fsol <- findCompBounded c fty (depth - 1)
+        return $ s ++ [o +> fsol]
+        )
+    sol
+    ope
+  return $ Comp cdir (Ty (d + 1) full)
+
+
+
+-- CSP SOLVER
 
 type Solving s a = StateT (SEnv s) [] a
 type Domain = [Term]
@@ -149,10 +185,10 @@ type CVar = Restr
 data CVarInfo a = CVarInfo { delayedConstraints :: Solving a (), values :: Domain }
 
 data SEnv s =
-  SEnv { ctxt :: Cube
+  SEnv { ctxt :: Ctxt
        , goal :: Ty
-       , dir :: Restr
-       , open :: [CVar]
+       , dir :: Restr -- in which direction do we want a comp
+       , open :: [CVar] --  the sides of the cubes that should be left open
        , varMap :: Map CVar (CVarInfo s)
        }
 
@@ -160,60 +196,6 @@ lookupDef :: Id -> Solving s Ty
 lookupDef name = do
   c <- gets ctxt
   return $ getDef c name
-
-constrOpenComp :: Cube -> Ty -> [[Restr]] -> Int -> [Term]
-constrOpenComp c ty@(Ty d _) opens fuel = do
-  let comp = (d + 1, I1)
-  ope <- opens
-  sol <- evalStateT runCS (SEnv c ty comp ope Map.empty)
-  full <- foldM
-    (\s o -> do
-        -- traceM "ALERT"
-        -- traceShowM s
-        let wty = Ty (d + 1) (s ++ [(d + 1 , I1) +> Fill comp ty])
-        -- traceShowM wty
-        let fty = computeOpenBoundary c wty o
-        -- traceShowM fty
-        -- unsafePerformIO (die "asd")
-        fsol <- findCompBounded c fty (fuel - 1)
-        -- traceShowM $ "FOUND" ++ show fsol
-        return $ s ++ [o +> fsol]
-        )
-    sol
-    ope
-
-  return $ Comp comp (Ty (d + 1) full)
-
-
-findCompBounded :: Cube -> Ty -> Int -> [Term]
-findCompBounded c ty 0 = constrOpenComp c ty [[]] 0
-
-findCompBounded c ty@(Ty d _) fuel =
-  let comp = (d + 1, I1) in
-  let sides = delete comp $ restrictions (d + 1) in
-  constrOpenComp c ty (incps sides) fuel
-    where
-    incps :: [a] -> [[a]]
-    incps = sortBy (comparing length) . filterM (const [True, False])
-
-
-
--- test = foldM (\s o -> do
---         fsol <- findCompBounded paths (Ty 2 [ (1,I0) +>    Deg (Var "x") 1
---                                             , (1,I1) +>    Var "p"
---                                             , (2,I0) +>    Deg (Var "x") 1
---                                             , (2,I1) +>    Var "p"]) 0
---         return $ s ++ [o +> fsol]
---         )
---     [((1,I0), Fill (2,I1) (Ty 2 [
---                           (1,I0) +>    Deg (Var "x") 1
---                         , (1,I1) +>    Var "p"
---                         , (2,I0) +>    Deg (Var "x") 1
---                         ])
---     ),((2,I0),Deg (Deg (Var "x") 1) 1),((2,I1),Deg (Var "p") 1),((3,I0),Deg (Deg (Var "x") 1) 1)]
---     [(1,I1)]
-
-
 
 runCS :: Solving s [Face]
 runCS = do
@@ -229,7 +211,7 @@ runCS = do
 
   sides <- mapM (\f@(i,_) ->
                       if i == gi || not (sideSpec ty f)
-                        then newCVar f pterms
+                        then newCVar f pterms -- if the side of the goal is not specified, we fill it in any way we want
                         else newCVar f (filter (\pt -> termFace c pt (gi-1,ge) == getFace ty f) pterms)
             )
         (faceInd \\ ope)
@@ -237,8 +219,7 @@ runCS = do
   -- domains <- mapM (\s -> lookupDom s >>= \r -> return (s , r)) sides
   -- traceM $ "AFTER INIT\n" ++ concatMap ((++ "\n") . show) domains ++ "END"
 
-  -- TODO just closed sides here?
-  mapM_ (uncurry boundaryConstraint) [ (f,g) | (f:ys) <- tails faceInd, g <- ys , fst f /= fst g ]
+  mapM_ (uncurry boundaryConstraint) [ (f,g) | (f:ys) <- tails (faceInd \\ ope), g <- ys , fst f /= fst g ]
 
   -- domains <- mapM (\s -> lookupDom s >>= \r -> return (s , r)) sides
   -- traceM $ "AFTER SIDE\n" ++ concatMap ((++ "\n") . show) domains ++ "END"
@@ -247,49 +228,34 @@ runCS = do
 
 
 
+-- Constraint management
+
 singleConstraint :: Restr -> CVar -> [Term] -> Solving s ()
 singleConstraint f cv hs = addConstraint cv $ do
   c <- gets ctxt
-  ope <- gets open
-  -- traceM $ show (pss) ++ "@" ++ show ie ++ " constrained to " ++ show hs
-  unless (cv `elem` ope) $ do
-    ts <- lookupDom cv
-    let ts' = filter (\t -> termFace c t f `elem` hs) ts
-    when (ts' /= ts) $ update cv ts'
+  ts <- lookupDom cv
+  let ts' = filter (\t -> termFace c t f `elem` hs) ts
+  when (ts' /= ts) $ update cv ts'
 
 boundaryConstraint :: Restr -> Restr -> Solving s ()
 boundaryConstraint = addBinaryConstraint $ \cv dv -> do
   c <- gets ctxt
-  ope <- gets open
-  -- traceM $ "MATCH " ++ show c ++ " WITH " ++ show d
-
-  -- TODO DOES THE BELOW MAKE SENSE?
   let (cf , df) = if fst cv < fst dv
         then (cv , (fst dv - 1 , snd dv))
         else ((fst cv - 1 , snd cv) , dv)
 
-  -- traceM $ "   AT " ++ show cf ++ " AND " ++ show df
+  ts <- lookupDom cv
+  ss <- lookupDom dv
+  let tsf = map (\t -> termFace c t df) ts
+  let ssg = map (\t -> termFace c t cf) ss
 
-  when (cv `notElem` ope && dv `notElem` ope) $ do
-      ts <- lookupDom cv
-      ss <- lookupDom dv
-      let tsf = map (\t -> termFace c t df) ts
-      let ssg = map (\t -> termFace c t cf) ss
-      -- traceShowM tsf
-      -- traceShowM ssg
+  let hs = tsf `intersect` ssg
 
-      let hs = tsf `intersect` ssg
-      -- traceShowM hs
+  let ts' = filter (\t -> termFace c t df `elem` hs) ts
+  let ss' = filter (\t -> termFace c t cf `elem` hs) ss
 
-      let ts' = filter (\t -> termFace c t df `elem` hs) ts
-      let ss' = filter (\t -> termFace c t cf `elem` hs) ss
-
-      -- traceShowM ts'
-      -- traceShowM ss'
-
-      when (ts' /= ts) $ update cv ts'
-      when (ss' /= ss) $ update dv ss'
-
+  when (ts' /= ts) $ update cv ts'
+  when (ss' /= ss) $ update dv ss'
 
 newCVar :: CVar -> Domain -> Solving s CVar
 newCVar v dom = do
@@ -344,19 +310,19 @@ getSol var = do
 
 
 
--- Examples
+-- EXAMPLES
+-- to run solver use findComp function, e.g., prove associativity with
+-- > findComp threep assoc
 
-tcomp :: Cube -> Term -> Term -> Term
+tcomp :: Ctxt -> Term -> Term -> Term
 tcomp c t t' = -- TODO CHECK IF COMPOSABLE
   Comp (2, I1) (Ty (termDim c t + 1) [
                      (1,I0) +> Deg (termFace c t (1,I0)) 1
                    , (1,I1) +> t'
                    , (2,I0) +> t ])
 
-
-
-paths :: Cube
-paths = [
+twop :: Ctxt
+twop = [
     ("x" , Ty 0 [])
   , ("y" , Ty 0 [])
   , ("z" , Ty 0 [])
@@ -435,8 +401,8 @@ unitl = Ty 2 [ (1,I0) +> reflp
 
 
 
-three :: Cube
-three = [
+threep :: Ctxt
+threep = [
     ("w" , Ty 0 [])
   , ("x" , Ty 0 [])
   , ("y" , Ty 0 [])
@@ -446,18 +412,51 @@ three = [
   , ("r" , Ty 1 [(1,I0) +> Var "y" , (1,I1) +> Var "z"])
       ]
 
-assocback = Ty 2 [ (1,I0) +> tcomp three (Var "p") (Var "q")
+assocback = Ty 2 [ (1,I0) +> tcomp threep (Var "p") (Var "q")
                  , (1,I1) +> Var "p"
                  , (2,I0) +> Deg (Var "w") 1
              ]
 
 assocright = Ty 2 [ (1,I0) +> Var "r"
-                  , (1,I1) +> tcomp three (Var "q") (Var "r")
+                  , (1,I1) +> tcomp threep (Var "q") (Var "r")
                   , (2,I1) +> Deg (Var "z") 1
              ]
 
-assoc = Ty 2 [ (1,I0) +> tcomp three (tcomp three (Var "p") (Var "q")) (Var "r")
-             , (1,I1) +> tcomp three (Var "p") (tcomp three (Var "q") (Var "r"))
+assoc = Ty 2 [ (1,I0) +> tcomp threep (tcomp threep (Var "p") (Var "q")) (Var "r")
+             , (1,I1) +> tcomp threep (Var "p") (tcomp threep (Var "q") (Var "r"))
              , (2,I0) +> Deg (Var "w") 1
              , (2,I1) +> Deg (Var "z") 1
              ]
+
+assoc2 = Ty 2 [ (1,I0) +> tcomp threep (Var "p") (tcomp threep (Var "q") (Var "r"))
+              , (1,I1) +> tcomp threep (tcomp threep (Var "p") (Var "q")) (Var "r")
+              , (2,I0) +> Deg (Var "w") 1
+              , (2,I1) +> Deg (Var "z") 1
+              ]
+
+assocOr = Ty 2 [ (1,I0) +> tcomp threep (Var "p") (tcomp threep (Var "q") (Var "r"))
+               , (1,I1) +>  Deg (Var "z") 1
+               , (2,I0) +> tcomp threep (tcomp threep (Var "p") (Var "q")) (Var "r")
+               , (2,I1) +> Deg (Var "z") 1
+              ]
+
+assocAnd = Ty 2 [ (1,I0) +>  Deg (Var "w") 1
+                , (1,I1) +> tcomp threep (Var "p") (tcomp threep (Var "q") (Var "r"))
+                , (2,I0) +> Deg (Var "w") 1
+                , (2,I1) +> tcomp threep (tcomp threep (Var "p") (Var "q")) (Var "r")
+              ]
+
+threep' :: Ctxt
+threep' = [
+    ("x" , Ty 0 [])
+  , ("p" , Ty 1 [(1,I0) +> Var "x" , (1,I1) +> Var "x"])
+  , ("q" , Ty 1 [(1,I0) +> Var "x" , (1,I1) +> Var "x"])
+  , ("r" , Ty 1 [(1,I0) +> Var "x" , (1,I1) +> Var "x"])
+      ]
+
+assoc' = Ty 2 [
+    (1,I0) +> tcomp threep' (tcomp threep' (Var "p") (Var "q")) (Var "r")
+  , (1,I1) +> tcomp threep' (Var "p") (tcomp threep' (Var "q") (Var "r"))
+  , (2,I0) +> Deg (Var "x") 1
+  , (2,I1) +> Deg (Var "x") 1
+      ]
